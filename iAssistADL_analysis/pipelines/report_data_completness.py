81c7303104b5bb5e07232652606d867ee7b475d8
moanos <julian-samuel@gebuehr.net>
2023-12-06 10:08:30 +0100

    feat: Add report on timestamp stats

---
+++ iAssistADL_analysis/pipelines/report_data_completness.py
@@ -0,0 +1,64 @@
+import h5py
+import pandas as pd
+from iAssistADL_analysis.tools import samples_lost
+
+
+def get_xsense_data(base_recording_path: str):
+    data = h5py.File(f"{base_recording_path}/xsense.h5", 'r')
+    return data
+
+
+def get_zed_data(base_recording_path: str):
+    data = h5py.File(f"{base_recording_path}/zed2i_skeleton3d.h5", 'r')
+    return data
+
+
+def get_gaze_data(base_recording_path: str):
+    data = pd.read_csv(f"{base_recording_path}/gaze.csv")
+    return data
+
+def get_xsense_timestamps(base_recording_path: str, sensor: str, ts_type: str = "sensor"):
+    data = get_xsense_data(base_recording_path)
+    if ts_type == "sensor":
+        return data["Sensors"][sensor]["Timestamp_Sensor"][0]
+    else:
+        return data["Sensors"][sensor]["Timestamp_Processing"][0]
+
+
+def get_zed_timestamps(base_recording_path: str, ts_type: str = "sensor"):
+    data = get_zed_data(base_recording_path)
+    if ts_type == "sensor":
+        return data["Timestamps"]["Timestamp_Sensor"][0]
+    else:
+        return data["Timestamps"]["Timestamp_Processing"][0]
+
+def get_gaze_timestamps(base_recording_path: str, ts_type: str = "sensor"):
+    data = get_gaze_data(base_recording_path)
+    if ts_type == "sensor":
+        return list(data["gaze_sensor_timestamps"])
+    else:
+        return list(data["gaze_processing_timestamps"])
+
+if __name__ == "__main__":
+    recording_base_path = "/home/moanos/Nextcloud/Masterarbeit/recordings/WI/1/5"
+    print("XSENSE")
+    for sensor in ["FOREARM_R", "HAND_R", "STERNUM", "UPPER_ARM_R"]:
+        timestamps = get_xsense_timestamps(recording_base_path, sensor)
+        samples_lost_abs, samples_lost_percentage = samples_lost(timestamps)
+        effective_sampling_frequency = len(timestamps) / (timestamps[-1] - timestamps[0])
+        print(f"For {sensor} {samples_lost_abs} datums seem to be lost which equates to {samples_lost_percentage * 100:.3}%.")
+        print(f"The sampling frequency overall was {effective_sampling_frequency:.4}Hz")
+
+    print("\nZED")
+    timestamps = get_zed_timestamps(recording_base_path)
+    samples_lost_abs, samples_lost_percentage = samples_lost(timestamps)
+    effective_sampling_frequency = len(timestamps) / (timestamps[-1] - timestamps[0])
+    print(f"For ZED3D {samples_lost_abs} datums seem to be lost which equates to {samples_lost_percentage * 100:.3}%.")
+    print(f"The sampling frequency overall was {effective_sampling_frequency:.4}Hz")
+
+    print("\nPUPIL")
+    timestamps = get_gaze_timestamps(recording_base_path)
+    samples_lost_abs, samples_lost_percentage = samples_lost(timestamps)
+    effective_sampling_frequency = len(timestamps) / (timestamps[-1] - timestamps[0])
+    print(f"For gaze data {samples_lost_abs} datums seem to be lost which equates to {samples_lost_percentage * 100:.3}%.")
+    print(f"The sampling frequency overall was {effective_sampling_frequency:.4}Hz")
