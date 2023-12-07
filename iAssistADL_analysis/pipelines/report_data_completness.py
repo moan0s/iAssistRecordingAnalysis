cb4cc942dbef208ecf9139b4362570072fc8b46f
moanos <julian-samuel@gebuehr.net>
2023-12-07 12:47:55 +0100

    feat: Add path together by schema, add seqIDs

--- iAssistADL_analysis/pipelines/report_data_completness.py
+++ iAssistADL_analysis/pipelines/report_data_completness.py
@@ -25,6 +25,9 @@ def get_xsense_timestamps(base_recording_path: str, sensor: str, ts_type: str = 
     else:
         return data["Sensors"][sensor]["Timestamp_Processing"][0]
 
+def get_xsense_seqIDs(base_recording_path: str, sensor: str):
+    data = get_xsense_data(base_recording_path)
+    return data["Sensors"][sensor]["SeqID"][0]
 
 def get_zed_timestamps(base_recording_path: str, ts_type: str = "sensor"):
     data = get_zed_data(base_recording_path)
@@ -41,11 +44,16 @@ def get_gaze_timestamps(base_recording_path: str, ts_type: str = "sensor"):
         return list(data["gaze_processing_timestamps"])
 
 if __name__ == "__main__":
-    recording_base_path = "/home/moanos/Nextcloud/Masterarbeit/recordings/WI/1/5"
+    subject = "Test"
+    session = "Completeness"
+    recording = "1"
+    base_data_path = "/home/moanos/Nextcloud/Masterarbeit/recordings/"
+    recording_base_path = f"{base_data_path}/{subject}/{session}/{recording}"
     print("XSENSE")
     for sensor in ["FOREARM_R", "HAND_R", "STERNUM", "UPPER_ARM_R"]:
         sensor_timestamps = get_xsense_timestamps(recording_base_path, sensor)
         processing_timestamps = get_xsense_timestamps(recording_base_path, sensor, "processing")
+        seqIDs = get_xsense_seqIDs(recording_base_path, sensor, "processing")
         time_diff_to_processing = processing_timestamps-sensor_timestamps
         samples_lost_abs, samples_lost_percentage = samples_lost(sensor_timestamps)
         effective_sampling_frequency = len(sensor_timestamps) / (sensor_timestamps[-1] - sensor_timestamps[0])
